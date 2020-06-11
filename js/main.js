7e67d722386b4a4c93526f54f4c9b0659b8e6520
Adedipe Toby <toby_adedipe@yahoo.com>
2020-06-11 23:22:38 +0100

    Finished sidebar and navbar

--- js/main.js
+++ js/main.js
@@ -1,6 +1,62 @@
-fetch('../data/data.json')
-    .then(res=> res.json())
-    .then(data=> console.log(data))
+// add class 'activeLink' to the current link in the browser
+const allLinks = document.querySelectorAll('a');
+// Set home link as default style
+const homeLink = document.querySelectorAll('a[href="/"]')
+homeLink.forEach(link=>{
+  link.className += ' active-link'
+});
+// create a function to remove the styling from other links and add it to currentlink
+function clickLink(e){
+  removeStyle();
+  this.classList.add('active-link')
+}
+// function to remove styling from other links
+function removeStyle(){
+  allLinks.forEach(link=> link.classList.remove('active-link'))
+}
+// add a clikc event to all the links on the page
+allLinks.forEach(link=> link.addEventListener('click', clickLink))
+
+async function getData(){
+  let response = await fetch('../data/data.json')
+  let data = await response.json()
+
+  return data
+}
+
+getData().then(data => {
+  let html = ""
+  var div = document.createElement('div')
+  // json.forEach(function (book){
+  //   html += "<p>"+ book.title + ""
+  // })
+  data.forEach(book=>{
+    html += `
+      <div class="all-books-section">
+        <div class="book-image">
+          <img src=${book.image} alt=${book.title}>
+        </div>
+        <div class="book-info">
+          <p>${book.availability}</p>
+          <p>${book.title}</p>
+          <p>${book.authors.map(category=>(
+            `<span>${category}</span>`
+          ))}
+          - ${book.yearPublished}</p>
+          <p>${book.category.map(category=>(
+            `<span>${category} </span>`
+          ))}
+          </p>
+          <p>${book.ratings}</p>
+          <p>${book.readers}</p>
+          <p>${book.likes}</p>
+      </div>
+    `;
+  })
+  div.innerHTML = html
+  document.getElementById("all-books").appendChild(div)
+})
+
 
     /*const json = JSON.parse(req.responseText);
     let html = "";
